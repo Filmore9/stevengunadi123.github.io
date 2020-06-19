882728e0236410ccc7557d69843b24277d7ca83b
stevengunadi123 <67083209+stevengunadi123@users.noreply.github.com>
2020-06-19 13:02:54 +0800

    Update index.js

--- script/index.js
+++ script/index.js
@@ -14,7 +14,5 @@ fetch(urlTrending,{
              document.getElementById("newsResults").innerHTML += "<div class='col l4 m6 s12'><div class='card-content'><span class='card-title activator'><i class='material-icons right'>more_vert</i></span><h6 class='truncate'>Title: <a href="+d.articles[i].link+" title="+d.articles[i].title+">"+d.articles[i].title+"</a></h6><p><b>Author</b>:" +d.articles[i].author+" </p><p><b>News source</b>:"+d.articles[i].topic+" </p><p><b>Published</b>:" +d.articles[i].published_date+ "</p></div><div class='card-reveal'><span class='card-title'><i class='material-icons right'>close</i></span><p><b>Description</b>:" +d.articles[i].summary+"</p></div><div class='card-action'><a href="+d.articles[i].link+" target='_blank' class='btn'>Read More</a></div></div></div>";
     }
 })
-.catch(err => {
-	console.log(err);
-});
+
 
