760b071ad9849f772d59706dbd1c2a861a55acf1
stevengunadi123 <67083209+stevengunadi123@users.noreply.github.com>
2020-06-19 13:14:32 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,53 @@
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
-            '/',
-            '/index.html',
-            '/manifest.json',
-            '/images/icons/app-icon-96x96.png',
-            '/images/icons/app-icon-144x144.png',
-            '/images/icons/app-icon-192x192.png',
-            '/images/icons/app-icon-384x384.png',
-            '/images/icons/app-icon-512x512.png'
-          ])
-        })
-    );
-    return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(res) {
-          return res;
-        })
-    );
-  });
+const STATIC_ASSETS = [ 
+ './',
+ './index.html',
+];
+
+const STATIC_CACHE_NAME = 'cache-static';
+
+self.addEventListener('install', async evt => {
+  const cache = await caches.open(STATIC_CACHE_NAME);
+  cache.addAll(STATIC_ASSETS);
+});
+
+self.addEventListener('fetch', evt => {
+ const req = evt.request;
+ const url = new URL(req.url);
+
+ if (url.origin == location.origin) {
+  evt.respondWith(cacheFirst(req));
+ } else {
+  evt.respondWith(networkFirst(req));
+ }
+});
+
+async function cacheFirst(req) {
+ const cachedResponse = await caches.match(req);
+ return cachedResponse || fetch(req);
+}
+
+async function networkFirst(req) {
+ const cache = await caches.open(STATIC_CACHE_NAME);
+ try {
+        // try go to network and fetch data 
+  const res = await fetch(req);
+  cache.put(req, res.clone());
+  return res;
+ } catch(error) {
+        // look something on cache. 
+  return await cache.match(req);
+ }
+}
+
+self.addEventListener('activate', (evt) => {
+ evt.waitUntil( 
+     caches.keys().then((keyList) => { 
+         return Promise.all(keyList.map((key) => { 
+             if (key !== STATIC_CACHE_NAME) { 
+                 console.log('[ServiceWorker] Removing old cache', key); 
+                 return caches.delete(key); 
+             } 
+         }));
+     })
+ ); 
+});
